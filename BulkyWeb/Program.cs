65eb3261ed74badfd5055574aee7bbec39997924
Abdul-Hanan-khan <hanankhanajk@gmail.com>
2025-01-05 22:41:15 +0500

    toastr notification added

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 // configure for entity core framework 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ));
