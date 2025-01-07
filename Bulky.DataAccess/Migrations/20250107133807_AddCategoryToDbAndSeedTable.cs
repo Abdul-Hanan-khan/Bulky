e2f82e3226db5a14e80b3ae6e0bbcc8fa915f600
Abdul-Hanan-khan <hanankhanajk@gmail.com>
2025-01-07 18:41:21 +0500

    Reset Database and Migrations

--- Bulky.DataAccess/Migrations/20250107133807_AddCategoryToDbAndSeedTable.cs
+++ Bulky.DataAccess/Migrations/20250107133807_AddCategoryToDbAndSeedTable.cs
@@ -2,10 +2,12 @@
 
 #nullable disable
 
-namespace BulkyWeb.Migrations
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class AddCategoryTableToDb : Migration
+    public partial class AddCategoryToDbAndSeedTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,12 +18,23 @@ namespace BulkyWeb.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "IsVerified", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, true, "Action" },
+                    { 2, 2, true, "SciFi" },
+                    { 3, 3, true, "History" }
                 });
         }
 
