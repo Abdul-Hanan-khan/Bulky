370dae0c5cc2b5ac8c398f39f2ee29027643a5f9
Abdul-Hanan-khan <hanankhanajk@gmail.com>
2025-01-19 15:33:59 +0500

    Payment Status Added

--- Bulky.Utilities/SD.cs
+++ Bulky.Utilities/SD.cs
@@ -12,5 +12,21 @@ namespace Bulky.Utilities
 		public const string Role_Company = "Company";
 		public const string Role_Admin = "Admin";
 		public const string Role_Employee = "Employee";
+
+		public const string Status_Pending = "Pending";
+		public const string Status_Approved = "Approved";
+		public const string Status_Processing = "Processing";
+		public const string Status_Shipped = "Shipped";
+		public const string Status_Cancelled = "Cancelled";
+		public const string Status_Refunded = "Refunded";
+
+		public const string Payment_Status_Pending = "Pending";
+		public const string Payment_Status_Approved = "Approved";
+		public const string Payment_Status_DelayedPayment = "ApprovedForDelayedPayment";
+		public const string Payment_Status_Rejected = "Rejected";
+
+
+
+
 	}
 }
