b885aee4cfe01488f27d0470cb38f34364a7cb2e
Abdul-Hanan-khan <hanankhanajk@gmail.com>
2025-01-19 08:12:25 +0500

    Cart Functionality Completed

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -11,65 +11,70 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
-	public class Repository<T> : IRepository<T> where T : class
-	{
-		private readonly ApplicationDbContext _db;
-		internal DbSet<T> dbSet;
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
         public Repository(ApplicationDbContext db)
         {
-			_db = db;
-			dbSet =  _db.Set<T>();
+            _db = db;
+            dbSet = _db.Set<T>();
         }
         void IRepository<T>.Add(T entity)
-		{
-			dbSet.Add(entity);
-		}
+        {
+            dbSet.Add(entity);
+        }
 
-		T IRepository<T>.Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
-		{
+        T IRepository<T>.Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
             IQueryable<T> query = dbSet;
 
-			if (!tracked)
-			{
-                 query = dbSet.AsNoTracking();
+            if (!tracked)
+            {
+                query = dbSet.AsNoTracking();
             }
-				
-				if (!string.IsNullOrEmpty(includeProperties))
-				{
-					foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-					{
-						query = query.Include(includeProp);
-					}
-				}
-				query = query.Where(filter);
-				return query.FirstOrDefault();
-			
-			
 
-		}
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            query = query.Where(filter);
+            return query.FirstOrDefault();
 
-		IEnumerable<T> IRepository<T>.GetAll(string ? includeProperties =null)  
-		{
+
+
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
+        {
             IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
 
-			if (!string.IsNullOrEmpty(includeProperties)) {
-                foreach (var includeProp in includeProperties.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries) )
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
                     query = query.Include(includeProp);
                 }
             }
 
-			return query.ToList();
-		}
+            return query.ToList();
+        }
 
-		void IRepository<T>.Remove(T entity)
-		{
-			dbSet.Remove(entity);
-		}
+        void IRepository<T>.Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
 
-		void IRepository<T>.RemoveRange(IEnumerable<T> entity)
-		{
-			dbSet.RemoveRange(entity);
-		}
-	}
+        void IRepository<T>.RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
 }
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartVM  ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;   
+        }
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userID = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartVM = new ShoppingCartVM
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCartRepo.GetAll(u=> u.ApplicationUserId == userID,includeProperties:"Product"),
+            };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = getPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+
+        public IActionResult plus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCartRepo.Get(u=>u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCartRepo.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult minus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCartRepo.Get(u=>u.Id == cartId);
+
+            if (cartFromDb.Count == 1)
+            {
+                _unitOfWork.ShoppingCartRepo.Remove(cartFromDb);
+            }
+            else {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCartRepo.Update(cartFromDb);
+                
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }    
+        public IActionResult remove(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCartRepo.Get(u=>u.Id == cartId);
+            
+            _unitOfWork.ShoppingCartRepo.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private double getPriceBasedOnQuantity(ShoppingCart cartItem) {
+            if (cartItem.Count <= 50)
+            {
+                return cartItem.Product.Price;
+            }
+            else {
+                if (cartItem.Count <= 100)
+                {
+                    return cartItem.Product.Price50;
+                }
+                else {
+                    return cartItem.Product.Price100;
+                }
+            }
         }
     }
 }
