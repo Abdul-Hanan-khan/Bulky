f588f868a6087e0e3185d956821b7d11103acb4f
Abdul Hanan <81586087+Abdul-Hanan-khan@users.noreply.github.com>
2025-06-04 16:16:07 +0500

    feat: add view component

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,14 +23,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-
-            if (claim != null)
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCartRepo.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-            }
+        
             IEnumerable<Product> productList = _unitOfWork.ProductRepo.GetAll(includeProperties: "Category");
             return View(productList);
         }
